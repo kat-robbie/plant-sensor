7b6311c66cad52e542cd3fbd57c8b1f522ad22c6
Kat Robbie <katherine.robbie@gmail.com>
2017-05-06 13:10:00 +1200

    basic jfive running

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  console.log("Arduino is ready!");
+
+  // Create a standard `led` component instance
+  var led = new five.Led(13);
+
+  // "blink" the led in 500ms
+  // on-off phase periods
+  led.blink(500);
+});
